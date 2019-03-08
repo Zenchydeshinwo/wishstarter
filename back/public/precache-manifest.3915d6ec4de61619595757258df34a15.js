99434b275129a5e6a2e12e17e50e39c851f528a5
Zenchy <alvaro.corral.soilan@gmail.com>
2019-03-08 11:35:41 +0100

    No parece que la seguridad de Giorgio mejore

---
+++ back/public/precache-manifest.3915d6ec4de61619595757258df34a15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3d885931941da3601ae3",
+    "url": "/static/js/main.3d885931.chunk.js"
+  },
+  {
+    "revision": "0a512412c46c5de44f1b",
+    "url": "/static/js/2.0a512412.chunk.js"
+  },
+  {
+    "revision": "3d885931941da3601ae3",
+    "url": "/static/css/main.d2f02793.chunk.css"
+  },
+  {
+    "revision": "07b65afbdf4152db84cfb9ef8572f87d",
+    "url": "/index.html"
+  }
+];
--- back/public/service-worker.js
+++ back/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87988a42b05b469a55fa53d6e8bf436f.js"
+  "/precache-manifest.3915d6ec4de61619595757258df34a15.js"
 );
 
 workbox.clientsClaim();

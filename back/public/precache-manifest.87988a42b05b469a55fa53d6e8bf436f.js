4b05bca8c342a6c1e001a98155f67deca59a060c
Zenchy <alvaro.corral.soilan@gmail.com>
2019-03-08 11:23:37 +0100

    Giorgio se esta jugando la vida

---
+++ back/public/precache-manifest.87988a42b05b469a55fa53d6e8bf436f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bb792469c39bace87f11",
+    "url": "/static/js/main.bb792469.chunk.js"
+  },
+  {
+    "revision": "4a99b721b6ebf7b33c9e",
+    "url": "/static/js/2.4a99b721.chunk.js"
+  },
+  {
+    "revision": "bb792469c39bace87f11",
+    "url": "/static/css/main.d2f02793.chunk.css"
+  },
+  {
+    "revision": "9915d88a107557ef03e0cf77cb32cf37",
+    "url": "/index.html"
+  }
+];
